f60dbc020f5adce4b3609a0db0bf580c3b918ea5
rachidelaid <rachidelaid@gmail.com>
2022-06-22 13:40:10 +0100

    styling text rotate component

--- components/TextRotate.jsx
+++ components/TextRotate.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/TextRotate.module.css';
 
-const ReactRotatingText = require('react-rotating-text')
+import ReactRotatingText from 'react-rotating-text';
 
-const TextRotate = () => {
-  return (
-    <div>
-      <p>&lt; <span>code</span> &gt;</p>
-      <p>I build <ReactRotatingText items={['web apps', 'mobile apps', 'desktop apps']} /></p>
-      <p>&lt; <span>code</span> &gt;</p>
-    </div>
-  )
-}
+const TextRotate = () => (
+  <div className={styles.text_rotate}>
+    <p>
+      &lt; <span>code</span> &gt;
+    </p>
+    <p className={styles.code}>
+      I build{' '}
+      <ReactRotatingText items={['web apps', 'mobile apps', 'desktop apps']} />
+    </p>
+    <p>
+      &lt; <span>code</span> &gt;
+    </p>
+  </div>
+);
 
-export default TextRotate
+export default TextRotate;
