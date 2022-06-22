9cc93f6e0fe6130c0440d2887723fdfb0938922a
rachidelaid <rachidelaid@gmail.com>
2022-06-22 16:17:33 +0100

    added address

--- components/SideDetails.jsx
+++ components/SideDetails.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import styles from '../styles/SideDetails.module.css';
 
 const SideDetails = () => {
-  return <div>SideDetails</div>;
+  const age = () => {
+    const dob = new Date('07/05/1997');
+    const month_diff = Date.now() - dob.getTime();
+    const age_dt = new Date(month_diff);
+    const year = age_dt.getUTCFullYear();
+    return Math.abs(year - 1970);
+  };
+
+  return (
+    <div className={styles.details}>
+      <ul>
+        <li>
+          <h5>Residence:</h5>
+          <span>Morocco</span>
+        </li>
+        <li>
+          <h5>City:</h5>
+          <span>Marrakech</span>
+        </li>
+        <li>
+          <h5>Age:</h5>
+          <span>{age()}</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default SideDetails;
