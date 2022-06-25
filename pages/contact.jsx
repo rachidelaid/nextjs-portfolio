63a88d7fb55b1efd830984e1924974cce0595c68
rachidelaid <rachidelaid@gmail.com>
2022-06-25 20:54:53 +0100

    animate formelements

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { FaUserAlt, FaAt, FaEnvelope } from 'react-icons/fa';
 import styles from '../styles/contact.module.css';
 
@@ -9,6 +9,18 @@ const cardMotions = {
   },
   visible: {
     scale: 1,
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const labelMotions = {
+  hidden: {
+    x: -30,
+  },
+  visible: {
+    x: 0,
   },
 };
 
@@ -19,42 +31,46 @@ const contact = () => {
 
   return (
     <div className={styles.container}>
-      <h2>Get in touch</h2>
-      <motion.form
-        onSubmit={handleSubmit}
-        className={styles.card}
-        variants={cardMotions}
-        initial="hidden"
-        animate="visible"
-        exit="hidden"
-      >
-        <motion.label htmlFor="name">
-          <FaUserAlt />
-          <input type="text" name="name" id="name" placeholder="name" />
-        </motion.label>
-        <motion.label htmlFor="email">
-          <FaAt />
-          <input type="email" name="email" id="email" placeholder="Email" />
-        </motion.label>
-        <motion.label htmlFor="message">
-          <FaEnvelope />
-          <textarea
-            name="message"
-            id="message"
-            cols="30"
-            rows="10"
-            placeholder="Message"
-          ></textarea>
-        </motion.label>
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          type="submit"
-          className={styles.btn}
+      <h3>Get in touch</h3>
+      <AnimatePresence>
+        <motion.form
+          onSubmit={handleSubmit}
+          className={styles.card}
+          variants={cardMotions}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
         >
-          Send Message
-        </motion.button>
-      </motion.form>
+          <motion.label variants={labelMotions} key="label1" htmlFor="name">
+            <FaUserAlt />
+            <input type="text" name="name" id="name" placeholder="name" />
+          </motion.label>
+          <motion.label variants={labelMotions} key="label2" htmlFor="email">
+            <FaAt />
+            <input type="email" name="email" id="email" placeholder="Email" />
+          </motion.label>
+          <motion.label variants={labelMotions} key="label3" htmlFor="message">
+            <FaEnvelope />
+            <textarea
+              name="message"
+              id="message"
+              cols="30"
+              rows="10"
+              placeholder="Message"
+            ></textarea>
+          </motion.label>
+          <motion.button
+            variants={labelMotions}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            type="submit"
+            key="button"
+            className={styles.btn}
+          >
+            Send Message
+          </motion.button>
+        </motion.form>
+      </AnimatePresence>
     </div>
   );
 };
