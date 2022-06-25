a61a59d39f9bbb09456b4ac93096255ab095205d
rachidelaid <rachidelaid@gmail.com>
2022-06-25 20:20:08 +0100

    adding the contact page

---
+++ pages/contact.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/contact.module.css';
+
+const contact = () => {
+  return <div className={styles.container}>contact</div>;
+};
+
+export default contact;
