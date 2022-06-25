d6d151e9e5ff27f07da198418deeeec09aac6c58
rachidelaid <rachidelaid@gmail.com>
2022-06-25 19:52:14 +0100

    DARY slider

--- components/Frameworks.jsx
+++ components/Frameworks.jsx
@@ -35,6 +35,35 @@ import 'swiper/css';
 import styles from '../styles/Frameworks.module.css';
 
 const Frameworks = () => {
+  const icons = [
+    <DiBootstrap key="icon1" />,
+    <DiCss3Full key="icon2" />,
+    <DiCss3 key="icon3" />,
+    <DiGit key="icon4" />,
+    <DiHtml5 key="icon5" />,
+    <DiJsBadge key="icon6" />,
+    <DiLinux key="icon7" />,
+    <DiMongodb key="icon8" />,
+    <DiNodejsSmall key="icon9" />,
+    <DiPostgresql key="icon10" />,
+    <DiPython key="icon11" />,
+    <DiRuby key="icon12" />,
+    <DiRor key="icon13" />,
+    <DiReact key="icon14" />,
+    <DiSass key="icon15" />,
+    <DiHeroku key="icon16" />,
+    <DiIonic key="icon17" />,
+    <SiD3Dotjs key="icon18" />,
+    <SiElectron key="icon19" />,
+    <SiFigma key="icon20" />,
+    <SiJest key="icon21" />,
+    <SiNetlify key="icon22" />,
+    <SiPostman key="icon23" />,
+    <SiRedux key="icon14" />,
+    <SiTypescript key="icon25" />,
+    <SiVisualstudiocode key="icon26" />,
+  ];
+
   return (
     <div className={styles.swiper}>
       <Swiper
@@ -43,112 +72,13 @@ const Frameworks = () => {
         loop={true}
         autoplay={{
           delay: 2500,
+          disableOnInteraction: false,
         }}
         modules={[Autoplay]}
       >
-        <SwiperSlide>
-          <DiBootstrap />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiCss3Full />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiCss3 />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiGit />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiHtml5 />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiJsBadge />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiLinux />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiMongodb />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiNodejsSmall />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiPostgresql />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiPython />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiRuby />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiRor />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiReact />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiSass />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiHeroku />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <DiIonic />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <SiD3Dotjs />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <SiElectron />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <SiFigma />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <SiJest />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <SiNetlify />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <SiPostman />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <SiRedux />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <SiTypescript />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <SiVisualstudiocode />
-        </SwiperSlide>
+        {icons.map((icon, index) => (
+          <SwiperSlide key={`icon-${index + 1}`}>{icon}</SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
