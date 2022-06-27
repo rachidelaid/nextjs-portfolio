fc380035ce2aa5f14f7897085575b538e0f7b59e
rachidelaid <rachidelaid@gmail.com>
2022-06-27 15:05:00 +0100

    recommendations custom pagination

--- components/Recommendations.jsx
+++ components/Recommendations.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import 'swiper/css/pagination';
 import styles from '../styles/Recommendations.module.css';
 
 const RecommendationCard = ({ card }) => {
@@ -48,20 +50,32 @@ const Recommendations = () => {
       description:
         'Rachid is one of those guys who is driven by passion and curiosity. His passion for writing clean and optimized code was clearly visible to me while we were working together. He has a good work ethic and professional behavior. He is always there whenever you need any kind of help. Also, he has a great aptitude for solving difficult problems. All in all, Rachid is a great guy. Recommended if you need to build a web or mobile app from the ground up.',
     },
+    {
+      name: 'REHMANMUNEEB UL REHMAN',
+      image:
+        'https://media-exp1.licdn.com/dms/image/C4D03AQE9L0-6W4MbUA/profile-displayphoto-shrink_100_100/0/1649156874342?e=1661990400&v=beta&t=91wzFF8oE_DF49JhFkth55W9wWEGYELtFhFywqxYIYo',
+      title: 'Software Engineer. Full-Stack developer (MERN / Rails)',
+      description:
+        'Rachid is one of those guys who is driven by passion and curiosity. His passion for writing clean and optimized code was clearly visible to me while we were working together. He has a good work ethic and professional behavior. He is always there whenever you need any kind of help. Also, he has a great aptitude for solving difficult problems. All in all, Rachid is a great guy. Recommended if you need to build a web or mobile app from the ground up.',
+    },
   ];
+
+  const pagination = {
+    clickable: true,
+    el: '#pagination',
+    renderBullet: (i, cn) =>
+      `<span class="${cn}" aria-label="Go to slide ${i}"></span>`,
+  };
 
   return (
     <div className={styles.recommendations}>
-      <h3>Recommendations</h3>
+      <h3>Recommendations From Linkedin</h3>
       <Swiper
         className={styles.slider}
         slidesPerView={2}
         spaceBetween={30}
-        loop={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
+        pagination={pagination}
+        modules={[Pagination]}
       >
         {recommendations.map((card, index) => (
           <SwiperSlide key={`card-${index}`} className={styles.slide}>
@@ -69,6 +83,7 @@ const Recommendations = () => {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className={styles.pagination} id="pagination"></div>
     </div>
   );
 };
