9ef2efa66696280abcf5bb2b30b2f9bf947a7204
rachidelaid <rachidelaid@gmail.com>
2022-06-27 17:23:42 +0100

    animate footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import styles from '../styles/Footer.module.css';
+
+const footerMotions = {
+  hidden: {
+    opacity: 0,
+    y: -100,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      ease: 'easeInOut',
+    },
+  },
+};
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
+    <motion.div
+      variants={footerMotions}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+      className={styles.footer}
+    >
       <p>© {new Date().getFullYear()} rachid el aid</p>
-    </div>
+    </motion.div>
   );
 };
 
