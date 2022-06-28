d94e2dbafaff2fa144a86d7a6dc805f1e9320ac0
rachidelaid <rachidelaid@gmail.com>
2022-06-28 17:04:51 +0100

    project details head

--- pages/projects/[id].jsx
+++ pages/projects/[id].jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import styles from '../../styles/Project.module.css';
 
-const Project = () => {
-  return <div>Project</div>;
+import Footer from '../../components/Footer';
+
+import projects from '../../utils/projects';
+
+export const getStaticPaths = () => {
+  return {
+    paths: projects.map((project) => ({
+      params: { id: project.id.toString() },
+    })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps = (context) => {
+  const { id } = context.params;
+  const project = projects.find((p) => p.id === +id);
+  return {
+    props: {
+      project,
+    },
+  };
+};
+
+const Project = ({ project }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.head}>
+        <h4>{project.title}</h4>
+        <div className={styles.category}>{project.langs.join(', ')}</div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Project;
