8fc5456836a46839241787ebd06b99b67c2b8137
rachidelaid <rachidelaid@gmail.com>
2022-06-28 13:22:09 +0100

    added projects filter

---
+++ pages/projects.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import styles from '../styles/projects.module.css';
+
+import Footer from '../components/Footer';
+import ProjectCard from '../components/ProjectCard';
+
+const projects = () => {
+  const [category, setCategory] = useState(0);
+
+  const categories = [
+    {
+      name: 'All',
+      id: 0,
+    },
+    {
+      name: 'Web',
+      id: 1,
+    },
+    {
+      name: 'Mobile',
+      id: 2,
+    },
+    {
+      name: 'Other',
+      id: 3,
+    },
+  ];
+
+  return (
+    <div>
+      <div className={styles.categories}>
+        {categories.map((cate) => (
+          <p
+            key={cate.id}
+            className={`${cate.id === category ? styles.active : ''}`}
+            onClick={() => setCategory(cate.id)}
+          >
+            {cate.name}
+          </p>
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default projects;
