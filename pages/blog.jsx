2be1b0588c9f79d2004628e9d9c45c16dc0aa482
rachidelaid <rachidelaid@gmail.com>
2022-06-30 11:41:49 +0100

    add the blog content

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import styles from '../styles/blog.module.css';
 
 import Footer from '../components/Footer';
+import ProjectCard from '../components/ProjectCard';
+
+import blogs from '../utils/blogs';
 
 const blog = () => {
   return (
-    <div>
-      <p>blog</p>
+    <div className={styles.container}>
+      <h4>Blogs</h4>
+
+      <div className={styles.blogs}>
+        {blogs.map((blog) => (
+          <ProjectCard key={blog.id} project={blog} />
+        ))}
+      </div>
       <Footer />
     </div>
   );
---
+++ utils/blogs.js
@@ -0,0 +1,11 @@
+const blogs = [
+  {
+    id: 1,
+    title: 'What is the best way to teach yourself web development?',
+    description: 'In this article, I will share what I think is the best way to teach yourself coding, as a self-thought web developer with five years of experience.',
+    image: '/blog-1.png',
+    link: 'https://medium.com/@elaidrachid/what-is-the-best-way-to-teach-yourself-web-development-72cf09816af'
+  }
+];
+
+export default blogs;
