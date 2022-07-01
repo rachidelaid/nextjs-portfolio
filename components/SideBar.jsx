7706366a77b199e154244348aa37b01d13324bd7
rachidelaid <rachidelaid@gmail.com>
2022-07-01 17:48:50 +0100

    adding the sidebar icon

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../styles/SideBar.module.css';
+import { FaEllipsisV } from 'react-icons/fa';
 
 import Avatar from './Avatar';
 import SideDetails from './SideDetails';
@@ -8,6 +9,9 @@ import SideFooter from './SideFooter';
 const SideBar = () => {
   return (
     <div className={styles.sidebar}>
+      <div className={styles.icon}>
+        <FaEllipsisV />
+      </div>
       <Avatar />
       <SideDetails />
       <SideFooter />
