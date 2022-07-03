98df9ec29d66497bd0578264fd5bb1b676a3cfdb
rachidelaid <rachidelaid@gmail.com>
2022-07-03 20:00:54 +0100

    fix footer to the bottom

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 import styles from '../../styles/projects.module.css';
 
 import Footer from '../../components/Footer';
@@ -7,7 +8,22 @@ import ProjectCard from '../../components/ProjectCard';
 // import projectList from '../../utils/projects';
 const projectList = [];
 
-const projects = () => {
+const noProjectsMotions = {
+  hidden: {
+    scale: 0,
+  },
+  visible: {
+    scale: 1,
+    transition: {
+      duration: 0.5,
+      ease: 'easeInOut',
+    },
+  },
+};
+
+const Projects = () => {
+  const [category, setCategory] = useState(0);
+
   const categories = [
     {
       name: 'All',
@@ -33,16 +49,23 @@ const projects = () => {
         {categories.map((cate) => (
           <p
             key={cate.id}
-            // className={`${cate.id === category ? styles.active : ''}`}
+            className={`${cate.id === category ? styles.active : ''}`}
+            onClick={() => setCategory(cate.id)}
           >
             {cate.name}
           </p>
         ))}
       </div>
       {!projectList.length && (
-        <div className={styles.no_projects}>
+        <motion.div
+          variants={noProjectsMotions}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          className={styles.no_projects}
+        >
           <p>No projects yet</p>
-        </div>
+        </motion.div>
       )}
       <div className={styles.projects}>
         {projectList.map((project) => (
@@ -54,4 +77,4 @@ const projects = () => {
   );
 };
 
-export default projects;
+export default Projects;
