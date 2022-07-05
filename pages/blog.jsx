c4e8822c9a3d615caf92a9456a4a89a83217afe3
rachidelaid <rachidelaid@gmail.com>
2022-07-05 21:02:34 +0100

    add exit animate to the blog page

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import styles from '../styles/blog.module.css';
+import { motion } from 'framer-motion';
 
 import Footer from '../components/Footer';
 import ProjectCard from '../components/ProjectCard';
 
 import blogs from '../utils/blogs';
 
+import { stagger, fadeInDown } from '../utils/motions';
+
 const blog = () => {
   return (
     <div className={styles.container}>
-      <h4>Blogs</h4>
+      <motion.h4
+        variants={fadeInDown}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+        Blogs
+      </motion.h4>
 
-      <div className={styles.blogs}>
+      <motion.div
+        variants={stagger}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        className={styles.blogs}
+      >
         {blogs.map((blog) => (
-          <ProjectCard key={blog.id} project={blog} blog={true} />
+          <motion.div variants={fadeInDown} key={blog.id}>
+            <ProjectCard project={blog} blog={true} />
+          </motion.div>
         ))}
-      </div>
-      <Footer position="relative" />
+      </motion.div>
+      <Footer position="absolute" />
     </div>
   );
 };
