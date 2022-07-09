2d8e2a55c172a9fde65f45593530a0f5cee231c4
rachidelaid <rachidelaid@gmail.com>
2022-07-09 19:34:46 +0100

    add light and dark mode

--- components/MenuBar.jsx
+++ components/MenuBar.jsx
@@ -14,6 +14,50 @@ const MenuBar = ({ openMenu }) => {
     setOpen(!open);
   };
 
+  const vars = [
+    '--dark1',
+    '--dark2',
+    '--dark3',
+    '--dark4',
+    '--dark5',
+    '--accent-color',
+    '--white-color',
+    '--gray-color',
+    '--lightgray-color',
+  ];
+  const darkVlaues = [
+    '#191923',
+    '#1e1e28',
+    '#20202a',
+    '#23232f',
+    '#2d2d39',
+    '#ffc107',
+    '#fafafc',
+    '#8c8c8e',
+    '#8c8c8e3b',
+  ];
+  const lightValues = [
+    '#ffffff',
+    '#eaeaea',
+    '#e0e0e0',
+    '#d1d1d1',
+    '#bababa',
+    '#ce0c2f',
+    '#070707',
+    '#161616',
+    '#fff',
+  ];
+
+  const toggleDark = () => {
+    setDark(!dark);
+    vars.forEach((variable, index) => {
+      document.documentElement.style.setProperty(
+        variable,
+        dark ? darkVlaues[index] : lightValues[index],
+      );
+    });
+  };
+
   return (
     <div className={`${styles.menu_bar} ${open ? styles.expand : ''}`}>
       <div className={styles.burger_menu} onClick={toggleMenu}>
@@ -26,10 +70,7 @@ const MenuBar = ({ openMenu }) => {
       <MenuLinks open={open} toggleMenu={toggleMenu} />
 
       <div className={styles.footer}>
-        <div
-          className={`${dark ? styles.active : ''}`}
-          onClick={() => setDark(!dark)}
-        >
+        <div className={`${dark ? styles.active : ''}`} onClick={toggleDark}>
           {dark ? <MdOutlineLightMode /> : <MdOutlineNightlight />}
         </div>
       </div>
