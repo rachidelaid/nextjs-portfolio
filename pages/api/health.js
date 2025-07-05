d590fe82410a68111cfa0ea9d274dabc6fbc53d2
rachidelaid <r.elaid@void.fr>
2025-07-05 20:47:47 +0100

    add health check API endpoint

---
+++ pages/api/health.js
@@ -0,0 +1,9 @@
+async function handler(req, res) {
+  if (req.method === "POST") {
+    res.status(200).json({ message: "OK" });
+  } else {
+    res.status(405).json({ error: "Method Not Allowed" });
+  }
+}
+
+export default handler;
