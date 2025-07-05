b87892bc0e739b684c4563b3396551e442cbf9c7
rachid el aid <42470159+rachidelaid@users.noreply.github.com>
2025-07-05 21:03:15 +0100

    Update health.js

--- pages/api/health.js
+++ pages/api/health.js
@@ -1,6 +1,6 @@
 async function handler(req, res) {
   if (req.method === "POST") {
-    res.status(200).json({ message: "OK" });
+    res.status(200).json({ message: "!OK" });
   } else {
     res.status(405).json({ error: "Method Not Allowed" });
   }
